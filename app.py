2e6a2548045ba2b1195a95d207421979ce5d39b9
khaiyra <44167602+khaiyra@users.noreply.github.com>
2021-01-15 10:40:33 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+import os
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def welcome_sca():
+    target = os.environ.get('TARGET' , 'Welcome to SCA Cloud School Application, this is my first assesment')
+    return '{}\n'.format(target)
+
+if __name__ == "__main__":
+    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
